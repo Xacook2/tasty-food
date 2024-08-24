b3248d9225de8fc56d69feefddac9aa76e1ab302
Xacook2 <XaviJenkins2012@gmail.com>
2024-08-24 13:37:24 +0200

    Update script.js

--- recipe/script.js
+++ recipe/script.js
@@ -1,4 +1,4 @@
-function storeValue() {
+afunction storeValue() {
     const sqlite3 = require('sqlite3').verbose();
     const db = new sqlite3.Database('database.db');
     const username = document.getElementById('username').value;
