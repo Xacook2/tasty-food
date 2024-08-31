37cecbad13a123188442ef0ff6b81be1a0227e66
Xacook2 <XaviJenkins2012@gmail.com>
2024-08-31 17:03:41 +0200

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+  username TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS rating (
+  username TEXT NOT NULL,
+  cottage_pie INTEGER 
+);
