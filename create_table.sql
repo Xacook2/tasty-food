c92d7dda0c8418afaa1a1098d7cd1ce737cdbfab
Xacook2 <XaviJenkins2012@gmail.com>
2024-09-14 16:33:44 +0200

    Delete create_table.sql

--- create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  username TEXT NOT NULL,
-  password TEXT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS rating (
-  username TEXT NOT NULL,
-  cottage_pie INTEGER 
-);
